a27d792322d5911a069a162409fd6dbb023aab7a
ahasanUllah <bulbulahasan@gmail.com>
2022-11-09 13:33:52 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: 'AIzaSyByJ4X_idKap673Xx9raYCLO6F6Z6qG8Vo',
-   authDomain: 'accounta-a4d1c.firebaseapp.com',
-   projectId: 'accounta-a4d1c',
-   storageBucket: 'accounta-a4d1c.appspot.com',
-   messagingSenderId: '346051957570',
-   appId: '1:346051957570:web:42ec6b1c7344b42160aedb',
+   apiKey: process.env.REACT_APP_apiKey,
+   authDomain: process.env.REACT_APP_authDomain,
+   projectId: process.env.REACT_APP_projectId,
+   storageBucket: process.env.REACT_APP_storageBucket,
+   messagingSenderId: process.env.REACT_APP_messagingSenderId,
+   appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
